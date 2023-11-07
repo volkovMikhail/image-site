e9a54ec1ea164091447c01120f513dc53a3cc576
volkovMikhail <volkov.work1@gmail.com>
2023-11-07 22:12:27 +0300

    start express structure

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+const { engine } = require('express-handlebars');
+
+app.engine('hbs', engine({extname: 'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'hello world', arr: ['hello', 'world'], partial: 'part' });
+});
+
+app.listen(port, () => {
+  console.log(`app listening at ${port}`);
+});
